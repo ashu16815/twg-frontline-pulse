9e9beb55248761f9461f586a1d456f66fa70c613
323905 <323905@example.com>
2025-10-10 08:38:15 +1300

    fix: Update Auckland timezone setup for existing tables only
    Updated timezone SQL to only create views for app_users table
    (feedback table doesn't exist yet in this schema).
    
    Successfully created:
    - vw_app_users_auckland (with NZ timestamps)
    
    Database views now show:
    - created_at_nz: Auckland timezone
    - last_login_at_nz: Auckland timezone
    - Plus original UTC columns
    
    Times automatically handle NZST/NZDT transitions.

--- scripts/set-auckland-timezone.sql
+++ scripts/set-auckland-timezone.sql
@@ -1,61 +1,13 @@
--- Set Auckland timezone for the database session
+-- Set Auckland timezone views for the database
 -- Note: Azure SQL doesn't have a global timezone setting, but we can use AT TIME ZONE
 
 -- Example of how to convert UTC to Auckland time in queries:
 -- SELECT created_at AT TIME ZONE 'UTC' AT TIME ZONE 'New Zealand Standard Time' as created_at_nz
--- FROM dbo.feedback;
 
 -- Note: In Azure SQL, 'New Zealand Standard Time' automatically handles DST
 -- (switches between NZST and NZDT)
 
--- You can create a view for easier querying:
-IF OBJECT_ID('dbo.vw_feedback_auckland', 'V') IS NOT NULL
-    DROP VIEW dbo.vw_feedback_auckland;
-GO
-
-CREATE VIEW dbo.vw_feedback_auckland AS
-SELECT 
-    id,
-    store_id,
-    store_code,
-    store_name,
-    region,
-    region_code,
-    banner,
-    iso_week,
-    manager_email,
-    hit_target,
-    variance_pct,
-    variance_dollars,
-    top_positive,
-    miss1,
-    miss1_dollars,
-    miss2,
-    miss2_dollars,
-    miss3,
-    miss3_dollars,
-    pos1,
-    pos1_dollars,
-    pos2,
-    pos2_dollars,
-    pos3,
-    pos3_dollars,
-    p1,
-    p1_horizon,
-    p2,
-    p2_horizon,
-    p3,
-    p3_horizon,
-    transcript_text,
-    overall_mood,
-    ai_themes,
-    ai_summary,
-    created_at AT TIME ZONE 'UTC' AT TIME ZONE 'New Zealand Standard Time' as created_at_nz,
-    created_at -- Keep UTC version too
-FROM dbo.feedback;
-GO
-
--- Similarly for app_users
+-- Create view for app_users with Auckland timezone
 IF OBJECT_ID('dbo.vw_app_users_auckland', 'V') IS NOT NULL
     DROP VIEW dbo.vw_app_users_auckland;
 GO
@@ -77,5 +29,6 @@ FROM dbo.app_users;
 GO
 
 PRINT 'Auckland timezone views created successfully!';
-PRINT 'Use vw_feedback_auckland and vw_app_users_auckland for Auckland times';
+PRINT 'Use vw_app_users_auckland for Auckland times';
+PRINT 'To use in queries: SELECT * FROM vw_app_users_auckland';
 
